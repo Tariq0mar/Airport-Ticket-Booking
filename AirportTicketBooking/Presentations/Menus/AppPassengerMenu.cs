0f6de1719a6acf3c77f13c97b600e2d87030e06a
Tariq Omar <tariqomar2004@gmail.com>
2025-04-18 18:48:43 +0300

    implement PassengerActions Menu

--- AirportTicketBooking/Presentations/Menus/AppPassengerMenu.cs
+++ AirportTicketBooking/Presentations/Menus/AppPassengerMenu.cs
@@ -1,6 +1,63 @@
-﻿namespace AirportTicketBooking.Presentations.Menus
+﻿using AirportTicketBooking.Enums;
+using AirportTicketBooking.Presentations.Actions.BookingActions;
+using AirportTicketBooking.Presentations.Actions.FlightActions;
+using AirportTicketBooking.Presentations.Actions.UserActions;
+
+namespace AirportTicketBooking.Presentations.Menus;
+
+public class AppPassengerMenu
 {
-    class AppPassengerMenu
+    public async Task Show()
     {
+        while (true)
+        {
+            Console.WriteLine("--- Passenger Menu ---");
+
+            foreach (var option in Enum.GetValues(typeof(PassengerOptions)))
+            {
+                Console.WriteLine($"{(int)option}. {option}");
+            }
+
+            Console.Write("Select an option: ");
+
+            if (!int.TryParse(Console.ReadLine(), out int input))
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+                Console.ReadKey();
+                continue;
+            }
+
+            var selectedOption = (PassengerOptions)input;
+
+            switch (selectedOption)
+            {
+                case PassengerOptions.GetUserData:
+                    await UserPassengerActions.GetUserById();
+                    break;
+
+                case PassengerOptions.GetFlight:
+                    await FlightPassengerActions.GetFlightById();
+                    break;
+
+                case PassengerOptions.GetAllFlights:
+                    await FlightPassengerActions.GetAllFlights();
+                    break;
+
+                case PassengerOptions.BookFlight:
+                    await BookingPassengerActions.AddBooking();
+                    break;
+
+                case PassengerOptions.MyBookings:
+                    await BookingPassengerActions.GetBookingsByPassengerId();
+                    break;
+
+                case PassengerOptions.Exit:
+                    return;
+
+                default:
+                    Console.WriteLine("Invalid option selected.");
+                    break;
+            }
+        }
     }
 }
